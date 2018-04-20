d8aa52d0fb4aac8e589c277d025055ac18231bb8
IKOMTK <h1201719004@hiratagakuen.onmicrosoft.com>
2018-04-20 11:23:14 +0900

    comii

--- CameraController.cs
+++ CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
     public GameObject player; // 玉のオブジェクト
 
     private Vector3 offset; // 玉からカメラまでの距離
-
+    //1239123
     void Start()
     {
         offset = transform.position - player.transform.position;
