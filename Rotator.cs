bfa870e23a40e5fdea3d74ddd68dd803df21f8c3
IKOMTK <h1201719004@hiratagakuen.onmicrosoft.com>
2018-04-20 11:24:49 +0900

    noo

---
+++ Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    void Update()//464556
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
